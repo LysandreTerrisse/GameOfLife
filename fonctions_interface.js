02a27e530e02d3cbff8c137c9eb8fa1fdb3f5f20
LysandreTerrisse <124552354+LysandreTerrisse@users.noreply.github.com>
2023-12-19 12:58:46 +0100

    Réussir la décomposition en plusieurs fichiers

--- fonctions_interface.js
+++ fonctions_interface.js
@@ -4,17 +4,17 @@ function enleverInputsCreationPartie() {
 
 function enleverInputsDeConnexion() {
     document.getElementById("menu_connexion").style.display = "none";
-    console.log("test");
-    document.getElementById("bouton_jouer"            ).type = "hidden"; // On enlève le bouton "Jouer" même s'il n'est pas dans le même <div>
+    document.getElementById("bouton_jouer").type = "hidden"; // On enlève le bouton "Jouer" même s'il n'est pas dans le même <div>
 }
 
 function extraireDepuisInterface() {
-    let nom_joueur    =        document.getElementById("champ_nom_joueur"   ).value ;
-    let nb_iterations = Number(document.getElementById("champ_nb_iterations").value);
-    let nb_joueurs    = Number(document.getElementById("champ_nb_joueurs"   ).value);
-    let nb_lignes     = Number(document.getElementById("champ_nb_lignes"    ).value);
-    let nb_colonnes   = Number(document.getElementById("champ_nb_colonnes"  ).value);
-    return [nom_joueur, nb_iterations, nb_joueurs, nb_lignes, nb_colonnes];
+    let nom_joueur            =        document.getElementById("champ_nom_joueur"           ).value ;
+    let nb_joueurs            = Number(document.getElementById("champ_nb_joueurs"           ).value);
+    let nb_entites_par_joueur = Number(document.getElementById("champ_nb_entites_par_joueur").value);
+    let nb_iterations         = Number(document.getElementById("champ_nb_iterations"        ).value);
+    let nb_lignes             = Number(document.getElementById("champ_nb_lignes"            ).value);
+    let nb_colonnes           = Number(document.getElementById("champ_nb_colonnes"          ).value);
+    return [nom_joueur, nb_joueurs, nb_entites_par_joueur, nb_iterations, nb_lignes, nb_colonnes];
 }
 
 //module.exports = {enleverInputsCreationPartie, enleverInputsDeConnexion};
