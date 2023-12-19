6a4174389944430a6501fbaf4b310fcdc2e2394e
LysandreTerrisse <124552354+LysandreTerrisse@users.noreply.github.com>
2023-12-19 18:30:59 +0100

    Ajouter des inputs range

--- fonctions_interface.js
+++ fonctions_interface.js
@@ -9,12 +9,29 @@ function enleverInputsDeConnexion() {
 
 function extraireDepuisInterface() {
     let nom_joueur            =        document.getElementById("champ_nom_joueur"           ).value ;
+    let force                 = Number(document.getElementById("champ_force"                ).value);
+    let perception            = Number(document.getElementById("champ_perception"           ).value);
+    let taux_reproduction     = Number(document.getElementById("champ_taux_reproduction"    ).value);
     let nb_joueurs            = Number(document.getElementById("champ_nb_joueurs"           ).value);
     let nb_entites_par_joueur = Number(document.getElementById("champ_nb_entites_par_joueur").value);
     let nb_iterations         = Number(document.getElementById("champ_nb_iterations"        ).value);
     let nb_lignes             = Number(document.getElementById("champ_nb_lignes"            ).value);
     let nb_colonnes           = Number(document.getElementById("champ_nb_colonnes"          ).value);
-    return [nom_joueur, nb_joueurs, nb_entites_par_joueur, nb_iterations, nb_lignes, nb_colonnes];
+    return [nom_joueur, force, perception, taux_reproduction, nb_joueurs, nb_entites_par_joueur, nb_iterations, nb_lignes, nb_colonnes];
 }
+
+/* S'exécute quand l'un des input de type range change */
+/* Affiche le bon nombre de points non répartis        */
+function updateStats() {
+    //On extrait les informations de l'interface
+    let [nom_joueur, force, perception, taux_reproduction, nb_joueurs, nb_entites_par_joueur, nb_iterations, nb_lignes, nb_colonnes] = extraireDepuisInterface();
+    //On modifie l'affichage des points non répartis, et des points de chaque statistique
+    document.getElementById("paragraphe_points_non_repartis").innerHTML = (9 - (force + perception + taux_reproduction));
+    document.getElementById("paragraphe_force").innerHTML = force;
+    document.getElementById("paragraphe_perception").innerHTML = perception;
+    document.getElementById("paragraphe_taux_reproduction").innerHTML = taux_reproduction;
+    
+}
+
 
 //module.exports = {enleverInputsCreationPartie, enleverInputsDeConnexion};
